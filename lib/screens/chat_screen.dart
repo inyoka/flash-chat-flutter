ceaab76bcd71255e98b5a6528e35f3a219057791
Simon <simonmarkholland@gmail.com>
2020-06-17 07:01:15 +0100

    Remove unused messagesStream

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -37,14 +37,6 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  void messagesStream() async {
-    await for (var snapshot in _firestore.collection('messages').snapshots()) {
-      for (var message in snapshot.documents) {
-        print(message.data);
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
