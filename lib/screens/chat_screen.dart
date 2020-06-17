2264acc7ed1a5b2d3306f60088cd8622e8505775
Simon <simonmarkholland@gmail.com>
2020-06-17 06:38:33 +0100

    Restore function of signout button

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -54,8 +54,8 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-//                _auth.signOut();
-//                Navigator.pop(context);
+                _auth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
